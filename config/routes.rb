cb39cd738c115e1f80524030b2810b6aa7f3267f
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-27 15:30:41 +0900

    firstcommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'items#index'
+  resources :items, only: [:index]
+end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
