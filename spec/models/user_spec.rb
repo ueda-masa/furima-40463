c204515532bebb0c50b858a58d428acb6e34399c
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-03-30 22:44:35 +0900

    テスト機能導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nameが空では登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.name = ''  # nameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Name can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.email = ''  # emailの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'passwordが空では登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.password = ''  # passwordの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Password can't be blank"
+    end
+    it 'first_name_kanjiが空では登録できない' do
+      user = FactoryBot.build(:user, first_name_kanji: '')  # Userのインスタンス生成
+      user.valid?
+      expect(user.errors.full_messages).to include "First name kanji can't be blank"
+    end
+
+    it 'family_name_kanjiが空では登録できない' do
+      user = FactoryBot.build(:user, family_name_kanji: '')  # Userのインスタンス生成
+      user.valid?
+      expect(user.errors.full_messages).to include "Family name kanji can't be blank"
+    end
+
+    it 'first_name_kanaが空では登録できない' do
+      user = FactoryBot.build(:user, first_name_kana: '')  # Userのインスタンス生成
+      user.valid?
+      expect(user.errors.full_messages).to include "First name kana can't be blank"
+    end
+
+    it 'family_name_kanaが空では登録できない' do
+      user = FactoryBot.build(:user, family_name_kana: '')  # Userのインスタンス生成
+      user.valid?
+      expect(user.errors.full_messages).to include "Family name kana can't be blank"
+    end
+
+    it 'birth_daysが空では登録できない' do
+      user = FactoryBot.build(:user, birth_days: '')  # Userのインスタンス生成
+      user.valid?
+      expect(user.errors.full_messages).to include "Birth days can't be blank"
+    end
+  end
 end
+
