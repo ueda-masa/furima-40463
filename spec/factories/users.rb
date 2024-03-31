614a98f3ae8edb83b044c989e43aae51e260a1c0
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-04-01 08:23:44 +0900

    パスワードにFaker記述とテスト内容追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    email                 { 'test@example.com' } # 有効なメールアドレスの形式に修正
-    password              { 'Password1' } # 半角英字と半角数字の組み合わせ
-    password_confirmation { 'Password1' } # パスワードと同じ値を設定
+    email { Faker::Internet.unique.email }
+    password              { 'Password1' } 
+    password_confirmation { 'Password1' } 
     name                  { 'Test User' }
     first_name_kanji      { '漢字名' }
     family_name_kanji     { '漢字姓' }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,56 @@ RSpec.describe User, type: :model do
       @user = FactoryBot.build(:user)
     end
 
+    describe 'ユーザー新規登録' do
+      it 'nameが空では登録できない' do
+        user = FactoryBot.build(:user)  # Userのインスタンス生成
+        user.name = ''  # nameの値を空にする
+        user.valid?
+        expect(user.errors.full_messages).to include "Name can't be blank"
+      end
+      it 'emailが空では登録できない' do
+        user = FactoryBot.build(:user)  # Userのインスタンス生成
+        user.email = ''  # emailの値を空にする
+        user.valid?
+        expect(user.errors.full_messages).to include "Email can't be blank"
+      end
+      it 'passwordが空では登録できない' do
+        user = FactoryBot.build(:user)  # Userのインスタンス生成
+        user.password = ''  # passwordの値を空にする
+        user.valid?
+        expect(user.errors.full_messages).to include "Password can't be blank"
+      end
+      it 'first_name_kanjiが空では登録できない' do
+        user = FactoryBot.build(:user, first_name_kanji: '')  # Userのインスタンス生成
+        user.valid?
+        expect(user.errors.full_messages).to include "First name kanji can't be blank"
+      end
+
+      it 'family_name_kanjiが空では登録できない' do
+        user = FactoryBot.build(:user, family_name_kanji: '')  # Userのインスタンス生成
+        user.valid?
+        expect(user.errors.full_messages).to include "Family name kanji can't be blank"
+      end
+
+      it 'first_name_kanaが空では登録できない' do
+        user = FactoryBot.build(:user, first_name_kana: '')  # Userのインスタンス生成
+        user.valid?
+        expect(user.errors.full_messages).to include "First name kana can't be blank"
+      end
+
+      it 'family_name_kanaが空では登録できない' do
+        user = FactoryBot.build(:user, family_name_kana: '')  # Userのインスタンス生成
+        user.valid?
+        expect(user.errors.full_messages).to include "Family name kana can't be blank"
+      end
+
+      it 'birth_daysが空では登録できない' do
+        user = FactoryBot.build(:user, birth_days: '')  # Userのインスタンス生成
+        user.valid?
+        expect(user.errors.full_messages).to include "Birth days can't be blank"
+      end
+    end
+
     # 正常系
     context '正常に登録できる場合' do
       it '全ての項目が入力されていれば登録できる' do
