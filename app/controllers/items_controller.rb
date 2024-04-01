f6a9d6e38475f856b3bd3620b066c868ff2475b5
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-04-01 19:12:33 +0900

    itemｺﾝﾄﾛｰﾗｰ内の保存する内容追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price ).merge(user_id: current_user.id)
   end
 
   def set_item
