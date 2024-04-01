95533fd39df656954a7b5051f7bd766879383f61
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-04-01 14:51:57 +0900

    テスト内容の文章修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
+    name { 'Test User' } # Remove one of the name definitions
     email { Faker::Internet.unique.email }
     password              { 'Password1' } 
     password_confirmation { 'Password1' } 
-    name                  { 'Test User' }
     first_name_kanji      { '漢字名' }
     family_name_kanji     { '漢字姓' }
     first_name_kana       { 'カナメイ' }
