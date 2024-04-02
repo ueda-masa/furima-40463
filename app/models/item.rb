3d0fb3bd03d1323faa11386419d368786f307ec6
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-04-02 19:21:34 +0900

    アクティブハッシュ導入途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,10 @@ class Item < ApplicationRecord
  validates :name, presence: true
  validates :description, presence: true
  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
- validates :condition_id, presence: true
- validates :shipping_charge_id, presence: true
- validates :prefecture_id, presence: true
- validates :shipping_day_id, presence: true
+ validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+ validates :shipping_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+ validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+ validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
  validates :price, presence: true
 
 end
