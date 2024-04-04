e8204417428e4ff053d199d1f3f0e6d7b770e130
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-04-04 15:12:46 +0900

    javascript導入完了

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,11 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
+const priceInput = document.getElementById("item-price"); // 商品価格のidをviewﾌｧｲﾙからもってくる
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price"); // 手数料のidをviewﾌｧｲﾙからもってくる
+  const taxValue = Math.floor(inputValue * 0.1); // これは手数料
+  addTaxDom.innerHTML = taxValue;
+
+  // 下記は出品部分に利益を表示するため
+  const profitDom = document.getElementById("profit");  // profitは販売利益、viewﾌｧｲﾙからもってくる
+  profitDom.innerHTML = inputValue - taxValue;
 });
