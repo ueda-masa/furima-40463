4340e07d958266fe6518bcebce5682276366f44d
ueda-masa <161990169+ueda-masa@users.noreply.github.com>
2024-04-08 19:25:27 +0900

    Basic認証の未記入部分

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
 
 
 
+  private
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
+  end
 
   private
   def configure_permitted_parameters
